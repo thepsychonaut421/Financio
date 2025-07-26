32a144f77385200bdbe8e28530eb87825494801e
Rares Balauta <rares.balauta19@gmail.com>
2025-07-26 01:30:12 +0000

    npm run build && npm run export studio-1079126013:~/studio{master}$ npm

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 const nextConfig: NextConfig = {
+  output: 'export',
   swcMinify: true,
   typescript: {
     ignoreBuildErrors: true,
