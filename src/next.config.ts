fd7bcd3c33f827b9c7730ba4412fe8f08cb735f8
Rares Balauta <rares.balauta19@gmail.com>
2025-07-26 01:38:33 +0000

    Error: Image Optimization using the default loader is not compatible wit

--- src/next.config.ts
+++ src/next.config.ts
@@ -28,8 +28,11 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
-      }
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
     ]
   }
 };
